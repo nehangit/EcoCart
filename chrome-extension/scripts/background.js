d5b55ecb3972d637a60b86037ef5dd2b1db8e44a
Hansen Zheng <zhansen1318@gmail.com>
2025-03-21 17:37:41 -0500

    Some minor changes

--- chrome-extension/scripts/background.js
+++ chrome-extension/scripts/background.js
@@ -9,21 +9,44 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 		// Get the current active tab
 		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			// Send message to content script running in the current active tab. Note that tabs[0] is the active tab
-			chrome.tabs.sendMessage(tabs[0].id, { action: "scrape" }, (response) => {
-				// Respond back to the popup.js after receiving data from content script
-				if (response) {
-					sendResponse({ success: true, data: response.productData });
-				} else {
-					sendResponse({ success: false, message: "No data received from content script." });
-				}
-			});
+			const url = tabs[0].url; // active tab's url
+
+			// Check if URL is valid on an allowed website (Amazon)
+			if (url.includes("amazon.com")) {
+
+				// Send message to content script running in the current active tab. 
+				// Note that tabs[0] is the active tab's id
+				chrome.tabs.sendMessage(tabs[0].id, { action: "scrape" }, (response) => {
+					// Respond back to the popup.js after receiving data from content script
+					if (response) {
+						// If content.js sends valid data
+						sendResponse({ success: true, data: response.productData });
+						sendDataToBackend(response.productData);
+					} else {
+						// If no data received from content.js
+						sendResponse({ success: false, message: "No data received from content script." });
+					}
+				});
+			} else { 
+				// alertUser("This extension is not supported on this website.");
+				// should update the extension UI 
+			}
+			
 		});
 		
 		// Tells Chrome to wait for sendResponse so the 'message port' does not close automatically
 		return true;
 	}
 });
+
+async function sendDataToBackend(productData) {
+	console.log("I'll come back to it another time ☺️")
+}
+
+function alertUser(message) {
+	// TODO
+}
+
 
 
 
