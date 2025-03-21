00ba6fd30ee3865b2bd6e52edde5389d3ca4df13
Hansen Zheng <zhansen1318@gmail.com>
2025-03-21 16:18:53 -0500

    Encapsulated scraping inside a function, modified regex to account for another product brand format.

--- chrome-extension/scripts/content.js
+++ chrome-extension/scripts/content.js
@@ -5,37 +5,41 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message.action === "scrape") {
 		console.log("Scrape action received. Extracting product data...");
 		
-		let product = {};
-
-		// Get product name 
-		product.name = document.getElementById("productTitle")?.textContent.trim() || null;
-
-		// Get brand name 
-		let brand = document.getElementById("bylineInfo")?.textContent.trim() || null;
-		product.brand = brand.replace(/^(Visit the )?(.*?)( Store)?$/i, "$2").trim();
-
-
-		// Get product facts (Information under "product-facts-title")
-		product.facts = {};
-
-		// Select all <li> elements (these have the fact-values)
-		let listItems = document.querySelectorAll("ul.a-nostyle > li");
-		listItems.forEach(item => {
-
-			// Select the 'fact' (key) and 'value' from within the <li>
-			let key = item.querySelector("div.a-col-left span.a-color-base")?.textContent.trim() || null;
-			let value = item.querySelector("div.a-col-right")?.textContent.trim() || null;
-
-			// Add the key-value pair to the facts object
-			if (key) {product.facts[key] = value;}
-		});
-
-		console.log("Extracted product data:", product);
-
-		// Send the extracted data back to background.js
-		sendResponse({ productData: product });
+		scrapeAmazonProduct(sendResponse);
+		
+		// Indicates that we using sendResponse asynchronously
+		return true;
 	}
 });
 
+function scrapeAmazonProduct(sendResponse) {
+	let product = {};
 
+	// Get product name 
+	product.name = document.getElementById("productTitle")?.textContent.trim() || null;
 
+	// Get brand name 
+	let brand = document.getElementById("bylineInfo")?.textContent.trim() || null;
+	product.brand = (brand) ? brand.replace(/^(?:Brand:\s*|Visit the )?(.*?)(?: Store)?$/i, "$1").trim() : null;
+	
+
+	// Get product facts (Information under "product-facts-title")
+	product.facts = {};
+
+	// Select all <li> elements (these have the fact-values)
+	let listItems = document.querySelectorAll("ul.a-nostyle > li");
+	listItems.forEach(item => {
+
+		// Select the 'fact' (key) and 'value' from within the <li>
+		let key = item.querySelector("div.a-col-left span.a-color-base")?.textContent.trim() || null;
+		let value = item.querySelector("div.a-col-right")?.textContent.trim() || null;
+
+		// Add the key-value pair to the facts object
+		if (key) {product.facts[key] = value;}
+	});
+
+	console.log("Extracted product data:", product);
+
+	// Send the extracted data back to background.js
+	sendResponse({ productData: product });
+}
